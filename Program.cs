49cec037a7727d131e01b643a3679015811a9327
EduardoGuedes06 <eduardoguedespsn@gmail.com>
2022-08-12 13:06:59 -0300

    Dapper

--- Program.cs
+++ Program.cs
@@ -1,33 +1,41 @@
 ﻿using System;
+using BaltaCourse.Models;
+using Dapper;
 using Microsoft.Data.SqlClient;
 
-namespace Balta_Course
+namespace BaltaCourse
 {
-
     class Program
     {
-
         static void Main(string[] args)
         {
-            const string ConnectionString = "Server=localhost,1433;Database=balta;User ID=sa;Passworld=1q2w3e4r@#$";
+            const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$;Encrypt=False";
 
-            var connection = new SqlConnection(ConnectionString);
+            using (var connection = new SqlConnection(connectionString))
             {
-                Console.WriteLine("Conectado");
-                connection.Open();
+                //connection.Open();
+                //Console.WriteLine("Conectado");
+                // using (var command = new SqlCommand())
+                // {
+                //     command.Connection = connection;
+                //     command.CommandType = System.Data.CommandType.Text;
+                //     command.CommandText = "SELECT [Id], [Title] FROM [Category]";
+                //     var reader = command.ExecuteReader();
+                //     while (reader.Read())
+                //     {
+                //         Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
+                //     }
 
-                using (var command = new SqlCommand())
-                {
-                    command.Connection = connection;
-                    command.CommandType = System.Data.CommandType.Text;
-                    command.CommandText = "SELECT [Id], [Title] FROM [Category]";
 
-                    var reader = command.ExecuteReader();
-                    while (reader.Read())
-                        Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
-                }
+                    //Usando o Dapper:
+                    var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+                    foreach(var category in categories)
+                    {
+                        Console.WriteLine($"{category.Id} - {category.Title}");
+                    }
+            
             }
-        }
 
+        }
     }
 }
