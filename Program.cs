a28b00d651850b14a2c18810a57a3873536d7844
EduardoGuedes06 <eduardoguedespsn@gmail.com>
2022-08-12 16:45:20 -0300

    Imersão

--- Program.cs
+++ Program.cs
@@ -11,26 +11,6 @@ namespace BaltaCourse
         {
             const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$;Encrypt=False";
 
-            var category = new Category();
-            category.Id = Guid.NewGuid();
-            category.Title = "Azure";
-            category.Url = "Amazon.com";
-            category.Description = "Destinado a AWS";
-            category.Order = 8;
-            category.Summary = "Zaas";
-            category.Featured = false;
-
-// SQL Injection
-            var insertSql = @"INSERT INTO 
-            [Category] 
-                VALUES(NEWID(),
-                    @Title, 
-                    @Url, 
-                    @Summary, 
-                    @Order, 
-                    @Description, 
-                    @Featured)";
-
             using (var connection = new SqlConnection(connectionString))
             {
                 //connection.Open();
@@ -45,28 +25,72 @@ namespace BaltaCourse
                 //     {
                 //         Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
                 //     }
-                {
-                   var rows = connection.Execute(insertSql, new{
-                        category.Id,
-                        category.Title,
-                        category.Url,
-                        category.Order,
-                        category.Summary,
-                        category.Description,
-                        category.Featured
-                    });
-                
-                    Console.WriteLine($"{rows} linhas inseridas");
+                //Usando o Dapper
 
-                }   //Usando o Dapper
-                var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
-                foreach(var item in categories)
-                {
-                    Console.WriteLine($"{item.Id} - {item.Title}");
-                }
+
+                UpdadteCategories(connection);
+                ListCategories(connection);
+                //CreateCategory(connection);
             
             }
 
         }
+
+
+        static void ListCategories(SqlConnection connection)
+        {
+            var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+            foreach (var item in categories)
+            {
+                Console.WriteLine($"{item.Id} - {item.Title}");
+            }
+        }
+
+
+        static void CreateCategory(SqlConnection connection)
+        {
+            var category = new Category();
+            category.Id = Guid.NewGuid();
+            category.Title = "Azure";
+            category.Url = "Amazon.com";
+            category.Description = "Destinado a AWS";
+            category.Order = 8;
+            category.Summary = "Zaas";
+            category.Featured = false;
+
+            // SQL Injection
+            var insertSql = @"INSERT INTO 
+            [Category] 
+                VALUES(NEWID(),
+                    @Title, 
+                    @Url, 
+                    @Summary, 
+                    @Order, 
+                    @Description, 
+                    @Featured)";
+            var rows = connection.Execute(insertSql, new
+            {
+                category.Id,
+                category.Title,
+                category.Url,
+                category.Order,
+                category.Summary,
+                category.Description,
+                category.Featured
+            });
+            Console.WriteLine($"{rows} linhas inseridas");
+        }
+
+        static void UpdadteCategories(SqlConnection connection)
+        {
+            var updateQuery = "UPDATE [category] SET [Title]=@title WHERE [Id]=@id";
+            var rows = connection.Execute(updateQuery, new
+            {
+                id = new Guid("d3894278-bf5c-4ce6-8a31-ae6bf0afaadd"),
+                title = "Forntend 2022"
+            });
+            Console.WriteLine("Registro Atualizado");
+        }
+
     }
 }
